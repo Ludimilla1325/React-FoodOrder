04d30de0dba469761adc16d311650acb3c46bb7d
Ludimilla <ludimillakrautzuk1325@gmail.com>
2022-05-25 10:03:29 -0300

    Meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react/cjs/react.production.min";
 import Header from "./components/Layout/Header";
+import Meals from "./Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
